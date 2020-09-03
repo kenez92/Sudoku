267206ee9425449d4fb360259814f46ff02bec95
Daniel Kaminski <kenez92@gmail.com>
2020-09-03 14:51:14 +0200

    SudokuGame and Main implementation

--- src/main/java/com/kenez92/Main.java
+++ src/main/java/com/kenez92/Main.java
@@ -2,6 +2,10 @@ package com.kenez92;
 
 public class Main {
     public static void main(String[] args) {
-
+        boolean gameFinished = false;
+        while (!gameFinished) {
+            SudokuGame theGame = new SudokuGame();
+            gameFinished = theGame.process();
+        }
     }
 }
--- src/main/java/com/kenez92/SudokuGame.java
+++ src/main/java/com/kenez92/SudokuGame.java
@@ -1,4 +1,43 @@
 package com.kenez92;
 
+import java.util.List;
+
 public class SudokuGame {
+    private final SudokuController sudokuController = new SudokuController();
+    private SudokuBoard sudokuBoard = new SudokuBoard(3);
+    private final SudokuResolver sudokuResolver = new SudokuResolver();
+
+    public SudokuBoard getSudokuBoard() {
+        return sudokuBoard;
+    }
+
+    public boolean process() {
+        sudokuBoard = sudokuController.createSudokuBoard();
+        System.out.println(sudokuBoard);
+        do {
+            if (putNumberIntoBoard(sudokuController.getPlayerMove(sudokuBoard.getSize()))) {
+                System.out.println(sudokuBoard);
+            } else {
+                sudokuController.badPlayerMove();
+            }
+        } while (sudokuBoard.countEmptyFields() > 0);
+        return true; // temporary return true
+    }
+
+    public boolean putNumberIntoBoard(List<Integer> playerMove) {
+        if (playerMove.size() == 0) {
+            sudokuResolver.process(sudokuBoard);
+        } else {
+            SudokuElement sudokuElement = sudokuBoard.getSudokuRowList()
+                    .get(playerMove.get(1) - 1)
+                    .getSudokuElementList().get(playerMove.get(0) - 1);
+            if (sudokuElement.getValue() == SudokuElement.EMPTY_VALUE) {
+                sudokuElement.setValue(playerMove.get(2));
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
 }
