e0f79e6b787206892d2533447898feff470b0a3a
Daniel Kaminski <kenez92@gmail.com>
2020-09-03 15:50:13 +0200

    Initial implementation SudokuResolver

--- src/main/java/com/kenez92/SudokuResolver.java
+++ src/main/java/com/kenez92/SudokuResolver.java
@@ -1,7 +1,29 @@
 package com.kenez92;
 
 public class SudokuResolver {
-    public boolean process(SudokuBoard sudokuBoard) {
-        return true; // temporary return true;
+    private final NumberRemover numberRemover = new NumberRemover();
+    private final SudokuConditions conditions = new SudokuConditions();
+
+    public void process(SudokuBoard sudokuBoard) {
+        boolean result = false;
+        boolean conditionResult = false;
+        do {
+            result = false;
+            conditionResult = false;
+            for (SudokuRow sudokuRow : sudokuBoard.getSudokuRowList()) {
+                for (SudokuElement sudokuElement : sudokuRow.getSudokuElementList()) {
+                    if (sudokuElement.getValue() == SudokuElement.EMPTY_VALUE) {
+                        numberRemover.excludeTakenNumbers(sudokuBoard, sudokuElement);
+                        conditionResult = conditions.process(sudokuElement, sudokuBoard);
+                        if (conditionResult == true && result == false) {
+                            result = true;
+                        }
+                    }
+                }
+            }
+            if (!conditionResult) {
+                // set random value in available number
+            }
+        } while (result);
     }
 }
